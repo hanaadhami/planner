c0675dc5f488c0afbf3d3bcd25dbea90fa433103
Hana <hanaadhami01@gmail.com>
2019-10-19 20:53:02 -0700

    adding my files

---
+++ script.js
@@ -0,0 +1,22 @@
+var now = moment();
+console.log();
+
+var content = "<planner>";
+
+for (var i = 0; i < 8; i++){
+
+} 
+    content += "</planner>";
+    content.addclass("time-block");
+    $("schedule").append(content);
+
+var table = $("<table>");
+
+<table>
+    <tr>
+        <td></td>
+    </tr>
+</table>
+
+content.append(table);
+
